d4e9e3997d62ef2705a4401f690cb19a6a03db89
Argxz <riyanargaw@gmail.com>
2024-12-11 10:11:35 +0700

    feat(testing):add dependencies jest + config jest

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(@mui|@emotion|framer-motion)/)/'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/**/index.js',
+  ],
+}
---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+// Import testing library
+import '@testing-library/jest-dom'
+
+// Nonaktifkan peringatan punycode
+process.env.NODE_OPTIONS = '--no-deprecation'
