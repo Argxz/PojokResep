3eedf92114c99555902467d16a82a099bc979c06
Argxz <riyanargaw@gmail.com>
2024-12-14 13:16:27 +0700

    feat(apps):implement clean code

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,21 +1,27 @@
+// src/routes/PrivateRoute.jsx
 import React, { useState, useEffect } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Swal from 'sweetalert2'
 
 const PrivateRoute = () => {
+  // State untuk mengatur loading spinner dan tampilan alert
   const [loadingSpin, setLoading] = useState(true)
   const [showAlert, setShowAlert] = useState(false)
 
+  // Mengatur timer untuk loading spinner
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false)
     }, 1000) // Delay 1 detik
 
-    return () => clearTimeout(timer) // Bersihkan timer
+    return () => clearTimeout(timer) // Bersihkan timer saat komponen unmount
   }, [])
+
+  // Mengambil state isAuthenticated dan loading dari Redux store
   const { isAuthenticated, loading } = useSelector((state) => state.auth)
 
+  // Mengatur alert jika pengguna tidak terautentikasi
   useEffect(() => {
     if (!loading && !isAuthenticated && !loadingSpin) {
       Swal.fire({
@@ -27,6 +33,7 @@ const PrivateRoute = () => {
     }
   }, [loading, isAuthenticated, loadingSpin])
 
+  // Menampilkan loading spinner jika masih dalam proses loading
   if (loading || loadingSpin) {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
@@ -36,10 +43,13 @@ const PrivateRoute = () => {
       </div>
     )
   }
+
+  // Jika alert ditampilkan, arahkan pengguna ke halaman login
   if (showAlert) {
     return <Navigate to="/login" replace />
   }
 
+  // Jika pengguna terautentikasi, render komponen anak
   return isAuthenticated ? <Outlet /> : null
 }
 
