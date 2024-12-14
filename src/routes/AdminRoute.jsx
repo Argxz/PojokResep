031dc136c6f50383955208108386fe355cbfd81b
Argxz <riyanargaw@gmail.com>
2024-12-14 13:15:17 +0700

    feat(apps):implement clean code

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -2,15 +2,19 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2' // Pastikan untuk mengimpor komponen LoadingSpinner
 
 const AdminRoute = () => {
+  // Mengambil state auth dari Redux store
   const { isAuthenticated, user, loading } = useSelector((state) => state.auth)
 
+  // Memeriksa apakah pengguna memiliki role 'admin'
   const isAdmin = user?.roles === 'admin'
 
+  // Menampilkan loading spinner jika data masih dimuat
   if (loading) return <LoadingSpinner />
 
+  // Jika pengguna tidak terautentikasi atau bukan admin, tampilkan peringatan dan arahkan ke halaman utama
   if (!isAuthenticated || !isAdmin) {
     Swal.fire({
       icon: 'warning',
@@ -21,6 +25,7 @@ const AdminRoute = () => {
     return <Navigate to="/" replace />
   }
 
+  // Jika pengguna terautentikasi dan merupakan admin, render komponen anak
   return <Outlet />
 }
 
